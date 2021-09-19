8d60a9961f6b5b95b9dc6aede2d6ed806335bdc7
Gabriel Teodoro <oi@gabrielteodoro.com>
2021-09-18 23:26:40 -0300

    feat: added theme based style guide

---
+++ src/styles/theme.ts
@@ -0,0 +1,58 @@
+export const theme = {
+  radius: {
+    input: '0.4rem',
+    card: '0.8rem',
+    background: '1.6rem',
+    rounded: '50%',
+  },
+  font: {
+    family: 'DM Sans',
+    weights: {
+      normal: 400,
+      medium: 500,
+      bold: 700,
+    },
+    sizes: {
+      disclaimer: '1.2rem',
+      small: '1.4rem',
+      paragraph: '1.6rem',
+      subtitle: '1.6rem',
+      title: '2.4rem',
+      headline: '3.2rem',
+    },
+  },
+  colors: {
+    white: '#fafafa',
+    blue: {
+      400: '#2dd4bf',
+      500: '#1fc8e1',
+    },
+    neutral: {
+      800: '#293445',
+      900: '#1e293b',
+    },
+  },
+  spacing: {
+    0: '0',
+    1: '0.4rem',
+    2: '0.8rem',
+    3: '1.6rem',
+    4: '2.4rem',
+    5: '3.2rem',
+    6: '4.0rem',
+    7: '4.8rem',
+    8: '5.6rem',
+    9: '6.4rem',
+  },
+  shadows: {
+    small: '0 0.9rem 1.8rem rgba(161, 161, 161, 0.1)',
+    large: '0 0.4rem 2.5rem rgba(0, 0, 0, 0.1)',
+    dropdown: '0 0.5rem 1rem rgba(0, 0, 0, 0.05)',
+    popover: '0 0.4rem 2.5rem rgba(0, 0, 0, 0.15)',
+  },
+  transition: {
+    slow: '0.5s ease-in-out',
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out',
+  },
+}
